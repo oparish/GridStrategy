b959c51093c218199c4fae6b46f4d1e1e87bf0ed
win7 pc <oparish4@gmail.com>
2014-03-26 22:22:19 +0000

    Normalised button widths.

--- GridStrategy/src/buttons/ControlButton.java
+++ GridStrategy/src/buttons/ControlButton.java
@@ -1,5 +1,6 @@
 package buttons;
 
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
@@ -7,12 +8,16 @@ import javax.swing.JButton;
 @SuppressWarnings("serial")
 public class ControlButton extends JButton
 {
+	private static final int CONTROLBUTTONWIDTH = 120;
+	private static final int CONTROLBUTTONHEIGHT = 30;
 	ButtonType buttonType;
 	
 	public ControlButton(ButtonType buttonType, ActionListener listener)
 	{
 		super(buttonType.getText());
 		this.buttonType = buttonType;
+		Dimension dimension = new Dimension(CONTROLBUTTONWIDTH, CONTROLBUTTONHEIGHT);
+		this.setPreferredSize(dimension);
 		this.addActionListener(listener);
 	}
 	
